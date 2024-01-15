3cf4dac884a22f3c35fa27d46757c831f066e935
syedmasoodrizvi <125534543+syedmasoodrizvi@users.noreply.github.com>
2024-01-15 08:07:25 -0800

    Update Task1.java

--- Task1.java
+++ Task1.java
@@ -3,7 +3,7 @@ package Lab_12;
 public class Task1 {
 	
 		public static void main(String args[]) { 
-		int[] number = {25,33,43,81,78,41,381,191,201};
+		int[] number = {25,33,43,8,8,41,381,191,201};
 		try{
 			System.out.println(number[1]);
 			System.out.println(number[41]);
